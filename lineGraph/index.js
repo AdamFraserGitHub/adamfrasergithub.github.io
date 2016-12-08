6b1cce3335f8909f7e7e8f1ca3de36d521df095d
AdamFraserGitHub <AdamFraserPrograming@gmail.com>
2016-12-08 19:41:55 +0000

    commit

--- lineGraph/index.js
+++ lineGraph/index.js
@@ -3,7 +3,7 @@ var x = 1, xDis = 0;
 var textOut = document.getElementById("output");
 var y, yDis, yDisTemp = 250;
 
-setInterval(loopydoo, 1000/90);
+setInterval(loopydoo, 1000/60);
 
 function loopydoo(){
     x+=0.5;
@@ -18,8 +18,10 @@ var c=document.getElementById("Canvas");
 var ctx=c.getContext("2d");
 
 ctx.beginPath();
+ctx.strokeStyle = 'rgb(0,0,0)';
 ctx.moveTo(xDis,yDisTemp);
 ctx.lineTo(xDis + 25,250 - yDis);
+ctx.closePath();
 ctx.stroke();
 
 yDisTemp = 250 - yDis
@@ -39,10 +41,14 @@ if (250 - yDis < 200){
 xDis +=25;
 
 ctx.beginPath();
-ctx.fillStyle = 'rgb(255,0,0)';
+ctx.strokeStyle = 'rgb(255,0,0)';
 ctx.moveTo(0,200);
-ctx.lineTo(1000,100);
+ctx.lineTo(15000,200);
+ctx.closePath();
 ctx.stroke();
+
+ctx.font = "20px Arial";
+ctx.fillText("200",5,180);
 }
 
 //xDis = scale
