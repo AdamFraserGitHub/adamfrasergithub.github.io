6afcbb83093b2d2a492ff2b0e1069ccd882b2311
AdamFraserGitHub <AdamFraserPrograming@gmail.com>
2016-12-22 00:55:58 +0000

    commit

--- ProceduralGenTest/JS/index.js
+++ ProceduralGenTest/JS/index.js
@@ -9,140 +9,153 @@ var charictar, ground,
 	gameStart = false,
 	yBoundryActive = [],
 	clearScreen = true,
-	debugPanel = false;
+	debugPanel = false,
+	blockMemory = [];
 
-	document.addEventListener('mousemove', funFunction, false);
+//urgh messy!
+blockMemory[0] = [];//xCoord
+blockMemory[1] = [];//yCoord
+blockMemory[2] = [];//type
+blockMemory[3] = [];//boundryBroken
 
-	//charictar properties
-	charictar = {
-		width:	10,
-		height:	10,
-		x:	scrWidth /10,
-		y:	scrHeight /2 -10 , //10 = charictar.height
-		fillColor:	'rgb(255,225,25)',
-		speed:	6,
-		jumpHeight:	6,
-		vX:	0,
-		vY:	0
-	};
+document.addEventListener('mousemove', funFunction, false);
 
-	//ground properties
-	ground = {
-		height: scrHeight /2,
-		width: 2,
-		fillColor: 'rgb(0,0,0)',
-	};
+//charictar properties
+charictar = {
+	width:	10,
+	height:	10,
+	x:	scrWidth /10,
+	y:	scrHeight /2 -10 , //10 = charictar.height
+	fillColor:	'rgb(255,225,25)',
+	speed:	6,
+	jumpHeight:	6,
+	vX:	0,
+	vY:	0
+};
 
-	setInterval(timer,	1000/30); 
-	
-	function timer(){
-		dynamicDeclerations();
-		draw();
-		controller();
-		boundryChecker();
-		physics();
-	}
+//ground properties
+ground = {
+	height: scrHeight /2,
+	width: 2,
+	fillColor: 'rgb(0,0,0)',
+};
 
-	function dynamicDeclerations(){
+setInterval(timer,	1000/30); 
 
-		if (canvas.width != window.innerWidth || canvas.height != window.innerHeight){
-			//updates scrWidth + scrHeight variables so game scale can change dynamicly
-			scrWidth = window.innerWidth;
-    		scrHeight = window.innerHeight;
-		
-			//reasigns canvas size
-			canvas.width = scrWidth
-			canvas.height = scrHeight
-		}
-	}
-
-	function draw(){
-
-		// clears the screen so last frame doesnt stay while adhering to the rainbow block effect
-		//(will find a better way to implement this cuz this is shite)
-		if (clearScreen){
-			ctx.clearRect(0,0,	scrWidth,scrHeight);
-		} else if (!clearScreen){
-			ctx.clearRect(0,0,	charictar.x - 20,charictar.y + charictar.height);
-			ctx.clearRect(0,charictar.y + charictar.height,	charictar.x,scrHeight);
-			ctx.clearRect(charictar.x, 0,	scrWidth - charictar.x,charictar.y + charictar.height);
-			ctx.clearRect(charictar.x, charictar.y + charictar.height,	scrWidth - charictar.x, scrHeight - (charictar.y - charictar.height));
-
-		}
-
-		//Draw charictar
-		if (yBoundryActive[4]){
-			charictarDraw.rainbowSkin();
-		} else {
-			ctx.fillStyle = charictar.fillColor;
-			ctx.fillRect(charictar.x,charictar.y,	charictar.width,charictar.height);
-		}
-
-		//Draw ground
-		ctx.fillStyle = ground.fillColor;
-		ctx.fillRect(0,ground.height,	scrWidth,ground.width);
-
-		if (debugPanel){
-			panel();
-		}
-
-		ctx.beginPath();
-ctx.fillStyle = 'rgb(0,255,0)';
-for (var i = 1; i < counter; i++){
-    ctx.rect(blockValues[0][i],blockValues[1][i] * 300 - 15,50,15);
+function timer(){
+	dynamicDeclerations();
+	draw();
+	controller();
+	boundryChecker();
+	physics();
 }
-ctx.fill();
-ctx.closePath();
+
+staticBlockMemory();
+
+function dynamicDeclerations(){
+
+	if (canvas.width != window.innerWidth || canvas.height != window.innerHeight){
+		//updates scrWidth + scrHeight variables so game scale can change dynamicly
+		scrWidth = window.innerWidth;
+		scrHeight = window.innerHeight;
+	
+		//reasigns canvas size
+		canvas.width = scrWidth
+		canvas.height = scrHeight
+	}
+}
+
+function draw(){
+
+	// clears the screen so last frame doesnt stay while adhering to the rainbow block effect
+	//(will find a better way to implement this cuz this is shite)
+	if (clearScreen){
+		ctx.clearRect(0,0,	scrWidth,scrHeight);
+	} else if (!clearScreen){
+		ctx.clearRect(0,0,	charictar.x - 20,charictar.y + charictar.height);
+		ctx.clearRect(0,charictar.y + charictar.height,	charictar.x,scrHeight);
+		ctx.clearRect(charictar.x, 0,	scrWidth - charictar.x,charictar.y + charictar.height);
+		ctx.clearRect(charictar.x, charictar.y + charictar.height,	scrWidth - charictar.x, scrHeight - (charictar.y - charictar.height));
 
 	}
 
+	//Draw charictar
+	if (yBoundryActive[4]){
+		charictarDraw.rainbowSkin();
+	} else {
+		ctx.fillStyle = charictar.fillColor;
+		ctx.fillRect(charictar.x,charictar.y,	charictar.width,charictar.height);
+	}
 
-	function controller(){
-		//left
-		if (keyPress[37]){
-			charictar.x -= charictar.speed;
-		}
+	//Draw ground
+	ctx.fillStyle = ground.fillColor;
+	ctx.fillRect(0,ground.height,	scrWidth,ground.width);
 
-		//right
-		if (keyPress[39]){
-			charictar.x += charictar.speed;
-		}
-
-		//up
-		if (keyPress[38] && charictar.y == ground.height - charictar.height){
-			charictar.vY = -charictar.jumpHeight;
-			gameStart = true;	
-			}
-		for(var i = 0; i < 5; i++){
-			if (keyPress[38] && yBoundryActive[i] == true){
-			charictar.vY = -charictar.jumpHeight;
-			}
-		}
+	if (debugPanel){
+		panel();
 	}
 
 
-	function boundryChecker(){
+	ctx.beginPath();
+	ctx.fillStyle = 'rgb(0,255,0)';
+	for (var i = 1; i < counter; i++){
+    	blockType[blockMemory[2][i]].x = blockMemory[0][i];
+		blockType[blockMemory[2][i]].y = blockMemory[1][i];
+
+		blockType[blockMemory[2][i]].drawBlock();
+	}
+	ctx.fill();
+	ctx.closePath();
+
+}
+
+
+function controller(){
+	//left
+	if (keyPress[37]){
+		charictar.x -= charictar.speed;
 	}
 
-
-	function physics(){
-
-		if (charictar.y <= ground.height - charictar.height){
-			charictar.y += charictar.vY;
-		}
-
-		if (charictar.vY != 0){
-			charictar.vY += Gravity;
-		} 
-
-		if (charictar.y >= ground.height -charictar.height){
-			charictar.y = scrHeight/2 - charictar.height;
-			charictar.vY = 0;
-		} else if (charictar.vY == 0 && gameStart == true && charictar.y < (scrHeight/2 + charictar.y)){
-			charictar.vY += Gravity;
-		}
-
+	//right
+	if (keyPress[39]){
+		charictar.x += charictar.speed;
 	}
+
+	//up
+	if (keyPress[38] && charictar.y == ground.height - charictar.height){
+		charictar.vY = -charictar.jumpHeight;
+		gameStart = true;	
+		}
+	for(var i = 0; i < 5; i++){
+		if (keyPress[38] && yBoundryActive[i] == true){
+		charictar.vY = -charictar.jumpHeight;
+		}
+	}
+}
+
+
+function boundryChecker(){
+}
+
+
+function physics(){
+
+	if (charictar.y <= ground.height - charictar.height){
+		charictar.y += charictar.vY;
+	}
+
+	if (charictar.vY != 0){
+		charictar.vY += Gravity;
+	} 
+
+	if (charictar.y >= ground.height -charictar.height){
+		charictar.y = scrHeight/2 - charictar.height;
+		charictar.vY = 0;
+	} else if (charictar.vY == 0 && gameStart == true && charictar.y < (scrHeight/2 + charictar.y)){
+		charictar.vY += Gravity;
+	}
+
+}
 
 
 //handles key presses
@@ -169,5 +182,16 @@ function funFunction(){
 		debugPanel = true;
 	} else {
 		debugPanel = false;
+	}
+}
+
+
+function staticBlockMemory(){
+	for (var i = 1; i < counter; i++){
+		blockMemory[0][i] = blockValues[0][i];
+		blockMemory[1][i] = blockValues[1][i] * 300 - 15;
+		blockMemory[2][i] = Math.floor(Math.random() * 5); //0~4 inclusive
+
+		// ctx.rect(blockValues[0][i],blockValues[1][i] * 300 - 15,50,15);
 	}
 }
