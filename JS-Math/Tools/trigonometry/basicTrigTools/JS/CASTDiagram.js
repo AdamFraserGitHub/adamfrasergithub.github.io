3e47066926876e9a3bf45bd384a8a38007a1d050
AdamFraserGitHub <AdamFraserPrograming@gmail.com>
2017-02-15 21:08:24 +0000

    commit

--- JS-Math/Tools/trigonometry/basicTrigTools/JS/CASTDiagram.js
+++ JS-Math/Tools/trigonometry/basicTrigTools/JS/CASTDiagram.js
@@ -1,46 +1,56 @@
 var CASTDiagramCanvas = document.getElementById("CASTDiagram"),
     CASTDiagramCtx = CASTDiagramCanvas.getContext('2d');
 
-var scrWidth = CASTDiagramCanvas.width,
-    scrHeight = CASTDiagramCanvas.height;
+var ctxWidth = CASTDiagramCanvas.width,
+    ctxHeight = CASTDiagramCanvas.height;
 
-//draws CAST diagram circular outline
-CASTDiagramCtx.beginPath();
-CASTDiagramCtx.strokeStyle = 'rgba(0,0,0,1)';
-CASTDiagramCtx.lineWidth = 1.5;
-CASTDiagramCtx.arc(scrWidth/2,scrHeight/2,scrWidth/2-25,0,Math.PI*2,false);
-CASTDiagramCtx.closePath();
-CASTDiagramCtx.stroke();
+setInterval(animation, 1000/30)
+function animation() {
+    CASTDiagramCtx.clearRect(0,0,ctxWidth,ctxHeight);
+    //draws CAST diagram circular outline
+    CASTDiagramCtx.lineWidth = 3;
+    CASTDiagramCtx.beginPath();
+    CASTDiagramCtx.strokeStyle = 'rgba(0,0,0,1)';
+    CASTDiagramCtx.arc(ctxWidth/2,ctxHeight/2,ctxWidth/2-25,0,Math.PI*2,false);
+    CASTDiagramCtx.closePath();
+    CASTDiagramCtx.stroke();
 
-//draws lines on CAST diagram
-CASTDiagramCtx.beginPath();
-CASTDiagramCtx.strokeStyle = 'rgba(255,0,0,1)';
-CASTDiagramCtx.lineWidth = 2;
-CASTDiagramCtx.moveTo(scrWidth/2,scrHeight/2);
-CASTDiagramCtx.lineTo(scrWidth/2 + scrWidth/2 - 15,scrHeight/2);
-CASTDiagramCtx.moveTo(scrWidth/2,scrHeight/2);
-CASTDiagramCtx.lineTo(scrWidth/2 - scrWidth/2 + 15,scrHeight/2);
-CASTDiagramCtx.moveTo(scrWidth/2,scrHeight/2);
-CASTDiagramCtx.lineTo(scrWidth/2,scrHeight/2 + scrHeight/2 - 15);
-CASTDiagramCtx.moveTo(scrWidth/2,scrHeight/2);
-CASTDiagramCtx.lineTo(scrWidth/2,scrHeight/2 - scrHeight/2 + 15);
-CASTDiagramCtx.closePath();
-CASTDiagramCtx.stroke();
+    //draws lines on CAST diagram
+    CASTDiagramCtx.beginPath();
+    CASTDiagramCtx.strokeStyle = 'rgba(255,0,0,1)';
+    CASTDiagramCtx.moveTo(ctxWidth/2,ctxHeight/2);
+    CASTDiagramCtx.lineTo(ctxWidth/2 + ctxWidth/2 - 15,ctxHeight/2);
+    CASTDiagramCtx.moveTo(ctxWidth/2,ctxHeight/2);
+    CASTDiagramCtx.lineTo(ctxWidth/2 - ctxWidth/2 + 15,ctxHeight/2);
+    CASTDiagramCtx.moveTo(ctxWidth/2,ctxHeight/2);
+    CASTDiagramCtx.lineTo(ctxWidth/2,ctxHeight/2 + ctxHeight/2 - 15);
+    CASTDiagramCtx.moveTo(ctxWidth/2,ctxHeight/2);
+    CASTDiagramCtx.lineTo(ctxWidth/2,ctxHeight/2 - ctxHeight/2 + 15);
+    CASTDiagramCtx.closePath();
+    CASTDiagramCtx.stroke();
+    
+    //draws line that follows angle
+    CASTDiagramCtx.beginPath();
+    CASTDiagramCtx.strokeStyle = 'rgba(150,0,255,1)';
+    CASTDiagramCtx.arc(ctxWidth/2,ctxHeight/2,75,0,(360-theta)/180*Math.PI, true);
+    CASTDiagramCtx.stroke();
+    CASTDiagramCtx.closePath();
+    
 
-//adds labels to CAST diagram
-CASTDiagramCtx.fillStyle = 'rgb(0,0,255)';
-CASTDiagramCtx.font = "12px Arial";
-CASTDiagramCtx.fillText("0°/360°",scrWidth - 67,scrHeight/2 - 5);
-CASTDiagramCtx.fillText("90°",scrWidth/2 -20,40);
-CASTDiagramCtx.fillText("180°",30,scrHeight/2 + 15);
-CASTDiagramCtx.fillText("270°",scrWidth/2 + 5,scrHeight - 30);
+    //adds labels to CAST diagram
+    CASTDiagramCtx.fillStyle = 'rgb(0,0,255)';
+    CASTDiagramCtx.font = "12px Arial";
+    CASTDiagramCtx.fillText("0°/360°",ctxWidth - 67,ctxHeight/2 - 5);
+    CASTDiagramCtx.fillText("90°",ctxWidth/2 -20,40);
+    CASTDiagramCtx.fillText("180°",30,ctxHeight/2 + 15);
+    CASTDiagramCtx.fillText("270°",ctxWidth/2 + 5,ctxHeight - 30);
 
-CASTDiagramCtx.font = "18px Arial";
-CASTDiagramCtx.fillText("x",scrWidth*2/3,scrHeight*1/3);
-CASTDiagramCtx.fillText("180 - x",scrWidth*1/3 - 20,scrHeight*1/3);
-CASTDiagramCtx.fillText("180 + x",scrWidth*1/3 - 20,scrHeight*2/3);
-CASTDiagramCtx.fillText("360 - x",scrWidth*2/3 - 20,scrHeight*2/3);
-
+    CASTDiagramCtx.font = "18px Arial";
+    CASTDiagramCtx.fillText("x",ctxWidth*2/3,ctxHeight*1/3);
+    CASTDiagramCtx.fillText("180 - x",ctxWidth*1/3 - 20,ctxHeight*1/3);
+    CASTDiagramCtx.fillText("180 + x",ctxWidth*1/3 - 20,ctxHeight*2/3);
+    CASTDiagramCtx.fillText("360 - x",ctxWidth*2/3 - 20,ctxHeight*2/3);
+}
 function controllsForThingy(e){
     var lool = true;
     if(e.clientX >= 200 && e.clientY <= 200 + 130){
--- JS-Math/Tools/trigonometry/basicTrigTools/JS/functionGraphDiagram.js
+++ JS-Math/Tools/trigonometry/basicTrigTools/JS/functionGraphDiagram.js
@@ -1,8 +1,8 @@
 var functionDiagramCanvas = document.getElementById("functionDiagram"),
     functionDiagramCtx = functionDiagramCanvas.getContext('2d');
 
-var scrWidth = functionDiagramCanvas.width - 100, //compensates for innacuracies caused by decimals not being acuratly represented in graphics
-    scrHeight = functionDiagramCanvas.height - 10; //so does this
+var functionCtxWidth = functionDiagramCanvas.width - 100, //compensates for innacuracies caused by decimals not being acuratly represented in graphics
+    fucntionCtxHeight = functionDiagramCanvas.height + 10; //so does this
 
 var theta = 0;
 
@@ -17,14 +17,15 @@ function timer(){
 function sineGraph() {
 
     functionDiagramCtx.clearRect(0,0,functionDiagramCanvas.width,functionDiagramCanvas.height)
-
+    
+    functionDiagramCtx.lineWidth = 2;
     functionDiagramCtx.beginPath();
-    functionDiagramCtx.moveTo(0,scrHeight/2 + 5);
+    functionDiagramCtx.moveTo(0,fucntionCtxHeight/2 + 5);
     for(var i = 0; i < 360; i++){
-        functionDiagramCtx.lineTo(i/360*scrWidth,scrHeight/2 - Math.sin(Math.PI*i/180) * scrHeight/2 + 5);
+        functionDiagramCtx.lineTo(i/360*functionCtxWidth,fucntionCtxHeight/2 - Math.sin(Math.PI*i/180) * fucntionCtxHeight/2 + 5);
     }
-    functionDiagramCtx.moveTo(0,scrHeight/2);
-    functionDiagramCtx.lineTo(scrWidth,scrHeight/2 + 5)
+    functionDiagramCtx.moveTo(0,fucntionCtxHeight/2 + 5);
+    functionDiagramCtx.lineTo(functionCtxWidth,fucntionCtxHeight/2 + 5)
     functionDiagramCtx.stroke();
     functionDiagramCtx.closePath();
     
@@ -32,28 +33,28 @@ function sineGraph() {
     
     //axis
     functionDiagramCtx.beginPath();
-    functionDiagramCtx.moveTo(1,scrHeight);
-    functionDiagramCtx.lineTo(1,0);
+    functionDiagramCtx.moveTo(2,fucntionCtxHeight );
+    functionDiagramCtx.lineTo(2,0);
     functionDiagramCtx.stroke();
     functionDiagramCtx.closePath();
     
     //labels
     functionDiagramCtx.font = "12px Arial";
     functionDiagramCtx.fillText("1",5,10);
-    functionDiagramCtx.fillText("0",5,scrHeight/2);
-    functionDiagramCtx.fillText("-1",5,scrHeight - 10);
+    functionDiagramCtx.fillText("0",5,fucntionCtxHeight/2);
+    functionDiagramCtx.fillText("-1",5,fucntionCtxHeight - 10);
 }
 
 //pointer system
 function pointerSystem() {
-    var x0 = theta/360 * scrWidth,
-        x1 = seccondAngle(theta,trigFunction,units)/360 * scrWidth,
-        y = scrHeight/2 - (Math.sin(theta/180 * Math.PI) * scrHeight/2) + 5;
+    var x0 = theta/360 * functionCtxWidth,
+        x1 = seccondAngle(theta,trigFunction,units)/360 * functionCtxWidth,
+        y = fucntionCtxHeight/2 - (Math.sin(theta/180 * Math.PI) * fucntionCtxHeight/2) + 5;
     
     //line
     functionDiagramCtx.beginPath();
     functionDiagramCtx.moveTo(0,y);
-    functionDiagramCtx.lineTo(scrWidth,y);
+    functionDiagramCtx.lineTo(functionCtxWidth,y);
     functionDiagramCtx.stroke();
     functionDiagramCtx.closePath();
     
